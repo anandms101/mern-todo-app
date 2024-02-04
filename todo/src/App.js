30a96797b144abe32500acba2c0c99336f445ce1
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-02-05 00:26:10 +0530

    displaying get data on the page

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
 import TodoItem from "./todoItem";
 
-const API_BASE= 'http://localhost:8080/todo';
+const API_BASE = "http://localhost:8080/todo";
 
 function App() {
-  
   const [items, setItems] = useState([]);
 
- useEffect(() => {
+  useEffect(() => {
     GetTodos();
   }, []);
 
- const GetTodos = () => {
-  fetch(API_BASE)
-  .then(res => res.json())
-  .then(data => setItems(data))
-  .catch(err => console.log(err))
- }
+  const GetTodos = () => {
+    fetch(API_BASE)
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="container">
@@ -25,17 +24,19 @@ function App() {
       </div>
 
       <div className="form">
-        <input type='text'></input>
+        <input type="text"></input>
         <button>
           <span>ADD</span>
         </button>
       </div>
 
-      <div className="todolist">  
-        <TodoItem/>  
+      <div className="todolist">
+        {items.map((item) => {
+          const { _id, name } = item;
+          return <TodoItem name={name} id={_id} setItems={setItems} />;
+        })}
       </div>
     </div>
- 
   );
 }
 
--- todo/src/todoItem.js
+++ todo/src/todoItem.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
+const API_BASE= 'http://localhost:8080/todo';
 
-function TodoItem(){
+function TodoItem(props){
+
+const {name, id, setItems} = props
     return(
      <div className="todo">
-        <div className="text">Test</div>
+        <div className="text">{name}</div>
         <div className="delete-todo"><span >X</span></div>
       </div>
     )
