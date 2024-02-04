3d3512c1e69f8ac246ebf7396cb6a35c84ea59d7
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-02-04 14:56:24 +0530

    formatted serverjs file

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const port = process.env.PORT || 3000;
+
+const connectionString = process.env.MONGO_URI;
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to the database…"))
+  .catch((err) => console.error("Connection error:", err));
+
 app.get("/todo", async (req, res) => {
   const todos = await todo.find();
   res.json(todos);
@@ -23,14 +31,6 @@ app.delete("/todo/delete/:id", async (req, res) => {
   const result = await todo.findByIdAndDelete(req.params.id);
   res.json(result);
 });
-
-const port = process.env.PORT || 3000;
-
-const connectionString = process.env.MONGO_URI;
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("Connected to the database…"))
-  .catch((err) => console.error("Connection error:", err));
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
