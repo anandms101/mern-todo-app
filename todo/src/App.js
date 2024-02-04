07ec48b2c90fc4bc8bb163aeabee2827b067c64d
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-02-05 00:21:40 +0530

    getting all tasks

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,23 @@
 import { useEffect, useState } from "react";
-import TodoItem from "./todoItem.js"
+import TodoItem from "./todoItem";
+
+const API_BASE= 'http://localhost:8080/todo';
 
 function App() {
+  
+  const [items, setItems] = useState([]);
+
+ useEffect(() => {
+    GetTodos();
+  }, []);
+
+ const GetTodos = () => {
+  fetch(API_BASE)
+  .then(res => res.json())
+  .then(data => setItems(data))
+  .catch(err => console.log(err))
+ }
+
   return (
     <div className="container">
       <div className="heading">
@@ -9,16 +25,17 @@ function App() {
       </div>
 
       <div className="form">
-        <input type="text"></input>
+        <input type='text'></input>
         <button>
           <span>ADD</span>
         </button>
       </div>
 
-      <div className="todolist">
-        <TodoItem />
+      <div className="todolist">  
+        <TodoItem/>  
       </div>
     </div>
+ 
   );
 }
 
