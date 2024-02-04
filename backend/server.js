0d4e6f98cc3f7156634a9ac7eb99ea85922ae87e
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2024-02-04 13:45:50 +0530

    database connection added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-require ('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+
+const connectionString = process.env.MONGO_URI;
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to the database…"))
+  .catch((err) => console.error("Connection error:", err));
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
